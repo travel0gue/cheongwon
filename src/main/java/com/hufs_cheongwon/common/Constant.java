5935c303d41bd152ca6bc6cffc859ae42f8e0fbd
sehyun <sese2204@naver.com>
2025-05-12 10:15:45 +0900

    청원 유지 기간 14일로 수정 왜 까먹었냐

--- src/main/java/com/hufs_cheongwon/common/Constant.java
+++ src/main/java/com/hufs_cheongwon/common/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
     public static final Integer REFRESH_COOKIE_EXPIRATION = 7 * 24 * 60 * 60; //7일
     public static final Integer EMAIL_COOKIE_EXPIRATION = 10 * 60; // 10분
 
-    public static final Period PETITION_ACTIVE_PERIOD = Period.ofDays(30);
+    public static final Period PETITION_ACTIVE_PERIOD = Period.ofDays(14);
 
     public static final Integer THRESHOLDAGREEMENT = 10;
 
